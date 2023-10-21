051e40cdb884c65ac1240ab05428c5da31cf4592
Hektor Zaimi <hektorzaimi.dev@gmail.com>
2023-10-21 18:45:10 +0200

    update routing system and prepare routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import Login from "./components/login";
-import {
-  Route,
-  Route,
-  Link,
-  Router,
- 
-} from "react-router-dom";
+import { useState } from "react";
+import { auth } from "./components/firebase-config";
 
+import Login from "./components/login";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { signOut } from "firebase/auth";
 function App() {
+
+  const [isAuth, setIsAuth] = useState(false)
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      
+      window.location.pathname = "/login";
+    });
+  };
   return (
-    <Router>
-      <Route></Route>
-      <div className="app">
-        <Login></Login>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="navbar bg-base-100">
+          <a className="btn btn-ghost normal-case text-xl mr-4">
+            social-media-app
+          </a>
+          <Link to="/" className="  normal-case text-lg mr-4">
+            Home
+          </Link>
+          <Link to="/login" className="  normal-case text-lg">
+            Login
+          </Link>
+          {isAuth ?  <button className="btn btn-error" onClick={signUserOut}>sign out</button> : <></> }
+         
+        </div>
+        <Routes>
+          <Route path="/" element={<div>test123</div>}></Route>
+          <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
+          
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import {auth, provider, signInPopup} from "./firebase-config";
-import {FcGoogle} from "react-icons/fc";
+import React, { useState } from "react";
+import { auth, provider, signInPopup } from "./firebase-config";
+import { FcGoogle } from "react-icons/fc";
 import "../App.css";
-function Login() {
-    const signInWithGoogle = () => {
-        signInPopup(auth, provider).then((result) => {
-            console.log(result)
-        }
-            
-        )
-    }
+import { useNavigate } from "react-router-dom";
+function Login({setIsAuth}) {
+  let navigate = useNavigate();
+  const signInWithGoogle = () => {
+    signInPopup(auth, provider).then((result) => {
+      console.log(result);
+      setIsAuth(true);
+      localStorage.setItem("isAuth", true)
+      navigate("/")
+    });
+  };
+
   return (
     <>
-    
-     <div className="Login flex justify-center content-center align-middle mt-60 ml-[30%] flex-col  bg-white">
-         <h1 className="font-extrabold text-4xl">Welcome, please log in: </h1>
-         
-         <button onClick={signInWithGoogle} className='a w-64 h-12  btn'> <FcGoogle className='a w-6 h-6'></FcGoogle> Sign in with google</button>
-    </div>
+      <div className="Login flex justify-center content-center align-middle mt-60 ml-[30%] flex-col  bg-white">
+        <h1 className="font-extrabold text-4xl">Welcome, please log in: </h1>
+
+        <button onClick={signInWithGoogle} className="a w-64 h-12  btn">
+          {" "}
+          <FcGoogle className="a w-6 h-6"></FcGoogle> Sign in with google
+        </button>
+      </div>
     </>
-   
-   
-  )
+  );
 }
 
-export default Login
+export default Login;
